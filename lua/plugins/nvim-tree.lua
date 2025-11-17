63c1056dcaaacc8d03a727c04fad866dd6ddf6a5
Isaac M <isaac@ikemcg.com>
2025-11-17 15:46:18 -0800

    Change nvim-tree config to make hidden files visible

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,12 +1,15 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  keys = {
-    { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle Nvim Tree" },
-  },
-  opts = {
-    filters = {
-        git_ignored = false,
-        dotfiles = true,
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+      { "<leader>e", ":NvimTreeToggle<CR>", desc = "Toggle Nvim Tree" },
     },
-  },
+    opts = {
+      filters = {
+          git_ignored = false,
+          dotfiles = false,
+      },
+      view = {
+        adaptive_size = true
+      }
+    },
 }
