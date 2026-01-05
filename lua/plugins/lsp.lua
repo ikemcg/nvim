1ca0f5fe45ff80993546bf2febd36b186e47d57d
Isaac M <isaac@ikemcg.com>
2026-01-05 09:14:26 -0800

    Use vtsls instead of ts_ls language server

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -214,22 +214,14 @@ return {
         eslint = {},
         jsonls = {},
         yamlls = {},
-        ts_ls = {},
-        vue_ls = {},
         marksman = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
-        --
-
+        vtsls = {},
+        vue_ls = {},
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -279,6 +271,26 @@ return {
           end,
         },
       })
+
+      -- IX: this didn't work when added to servers array above
+      vim.lsp.config('vtsls', {
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                {
+                  name = '@vue/typescript-plugin',
+                  location = vim.fn.expand '$MASON/packages' .. '/vue-language-server' .. '/node_modules/@vue/language-server',
+                  languages = { 'vue' },
+                  configNamespace = 'typescript',
+                },
+              },
+            },
+          },
+        },
+      });
+      vim.lsp.enable('vtsls')
     end,
   },
 }
