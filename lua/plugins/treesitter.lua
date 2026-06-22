5df2df80deb6b730b00f6027627d81840840f9e2
Isaac M <isaac@ikemcg.com>
2026-06-22 14:02:56 -0700

    Update Treesitter config so it conforms with newest versions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,41 +1,47 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    main = "nvim-treesitter.configs", -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    opts = {
-      ensure_installed = {
-        "bash",
-        "diff",
-        "html",
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "php",
-        "javascript",
-        "json",
-        "terraform",
-        "vue",
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { "ruby" },
-      },
-      indent = { enable = true, disable = { "ruby" } },
-    },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-  },
+    version = false, -- Use the latest fixes from the main branch
+    build = ":TSUpdate", -- Automatically update parsers when the plugin updates
+    lazy = false, -- Core plugin feature; must load on startup
+    config = function()
+      -- The configuration API uses 'nvim-treesitter.configs'
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        -- A list of parser names to install automatically upon setup
+        ensure_installed = {
+					"bash",
+					"diff",
+					"html",
+					"css",
+					"lua",
+					"luadoc",
+					"markdown",
+					"markdown_inline",
+					"query",
+					"php",
+					"javascript",
+					"typescript",
+					"json",
+					"terraform",
+					"vue",
+        },
+
+        -- Install missing parsers silently on the fly when opening unhandled files
+        auto_install = true,
+
+        -- Advanced syntax highlighting config
+        highlight = {
+          enable = true, -- Enable native treesitter-based text highlighting
+          additional_vim_regex_highlighting = false, -- Disable slow Vim regex fallbacks
+        },
+
+        -- Semantic code indentation config
+        indent = {
+          enable = true
+        },
+      })
+    end,
+  }
 }
