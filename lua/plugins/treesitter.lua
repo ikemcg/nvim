ee0fb18aae048392bdee6068e51d03f7ce66c527
Isaac M <isaac@ikemcg.com>
2026-07-21 08:51:13 -0700

    Revert previous change to treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
     lazy = false, -- Core plugin feature; must load on startup
     config = function()
       -- The configuration API uses 'nvim-treesitter.configs'
-      local configs = require("nvim-treesitter.config")
+      local configs = require("nvim-treesitter.configs")
 
       configs.setup({
         -- A list of parser names to install automatically upon setup
