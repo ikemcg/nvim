99d8dd2997684881709d5fe50d9c0f84b957fa54
Isaac M <isaac@ikemcg.com>
2026-08-24 16:07:37 -0700

    Add ansible filetype detection

--- init.lua
+++ init.lua
@@ -8,6 +8,16 @@ vim.g.have_nerd_font = true
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- properly identify ansible yaml files
+vim.filetype.add({
+  pattern = {
+    ['.*/defaults/.*%.ym[l]'] = 'yaml.ansible',
+    ['.*/vars/.*%.ym[l]'] = 'yaml.ansible',
+    ['.*/group_vars/.*'] = 'yaml.ansible',
+    ['.*/host_vars/.*'] = 'yaml.ansible',
+  },
+})
+
 require("config.keymaps")
 require("config.options")
 require("config.autocmd")
